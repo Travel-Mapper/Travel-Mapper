d562d33e54846a4d8183d3a7d7fdafe49a01c75d
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2024-09-14 17:34:44 +0900

    Feat: 추가. 메일 보내기 테스트

---
+++ study/src/main/java/com/study/login/MailTest.java
@@ -0,0 +1,49 @@
+package com.study.login;
+
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+public class MailTest {
+
+    public static void main(String[] args) throws MessagingException {
+        MailTest mailTest = new MailTest();
+        mailTest.test();
+    }
+
+    public void test() throws MessagingException {
+
+        String to = "9669579@gmail.com"; // 받는 사람의 이메일 주소
+        String from = "9669579@naver.com"; // 보내는 사람의 이메일 주소
+        String password = ""; // 보내는 사람의 이메일 계정 비밀번호
+        String host = "smtp.naver.com"; // 구글 메일 서버 호스트 이름
+
+        // SMTP 프로토콜 설정
+        Properties props = new Properties();
+        props.setProperty("mail.smtp.host", host);
+        props.setProperty("mail.smtp.port", "587");
+        props.setProperty("mail.smtp.auth", "true");
+        props.setProperty("mail.smtp.starttls.enable", "true");
+        props.setProperty("mail.smtp.starttls.enable", "true"); // TLS를 활성화합니다.
+
+
+        // 보내는 사람 계정 정보 설정
+        Session session = Session.getInstance(props, new Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(from, password);
+            }
+        });
+
+        // 메일 내용 작성
+        Message msg = new MimeMessage(session);
+        msg.setFrom(new InternetAddress(from));
+        msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
+        msg.setSubject("메일 제목");
+        msg.setText("메일 내용");
+
+        // 메일 보내기
+        Transport.send(msg);
+    }
+}
