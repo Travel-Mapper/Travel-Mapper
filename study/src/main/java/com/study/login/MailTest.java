7cfd996d274aa467b5e706df8cdfbda4cb13431b
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2024-09-15 17:59:06 +0900

    Feat: 추가. html 메일로 보내기

--- study/src/main/java/com/study/login/MailTest.java
+++ study/src/main/java/com/study/login/MailTest.java
@@ -4,16 +4,20 @@ package com.study.login;
 import javax.mail.*;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Properties;
 
 public class MailTest {
 
-    public static void main(String[] args) throws MessagingException {
+    public static void main(String[] args) throws MessagingException, IOException {
         MailTest mailTest = new MailTest();
         mailTest.test();
     }
 
-    public void test() throws MessagingException {
+    public void test() throws MessagingException, IOException {
 
         String to = "9669579@gmail.com"; // 받는 사람의 이메일 주소
         String from = "9669579@naver.com"; // 보내는 사람의 이메일 주소
@@ -36,12 +40,14 @@ public class MailTest {
             }
         });
 
+        String html = new String(Files.readAllBytes(Paths.get("C:\\Users\\hojun\\Desktop\\Git\\Travel-Mapper\\study\\src\\main\\resources\\templates\\QRpage.html")), "UTF-8");
+
         // 메일 내용 작성
         Message msg = new MimeMessage(session);
         msg.setFrom(new InternetAddress(from));
         msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
-        msg.setSubject("메일 제목");
-        msg.setText("메일 내용");
+        msg.setSubject("ttt");
+        msg.setContent(html,"text/html; charset=utf-8");
 
         // 메일 보내기
         Transport.send(msg);
